502e66600f6778b28c7f489b53045e786d67577b
Alef Fabricio <alef.fcg@gmail.com>
2019-09-24 00:08:28 -0300

    atualizado o tamanho da logo e agora limpa o campo

--- estacionamento/src/app/paginas/veiculo/veiculo.component.ts
+++ estacionamento/src/app/paginas/veiculo/veiculo.component.ts
@@ -49,8 +49,8 @@ export class VeiculoComponent implements OnInit {
       && this.veiculo.placa != null){
         console.log(this.veiculo);
         this.veiculoService.salvarVeiculo(this.veiculo).subscribe((veiculo: Veiculo) => { 
-          // this.veiculo = new Veiculo;
-          this.veiculo = veiculo;
+          this.veiculo = new Veiculo;
+          // this.veiculo = veiculo;
           alert('Veiculo salvo com sucesso');
          });
       }else{
